a3942054df52aa266f0bc26f17098b25831012e2
GrimmOutlook <grimmoutlook87@gmail.com>
2018-04-23 10:57:45 -0400

    Completed maxCharexercise + all tests passing.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const strArray = str.split('');
+  const obj = {};
+  let max = 0;
+  let maxChar = '';
+
+  for(char of strArray){
+    // obj[char] = obj[char] + 1 || 1;
+    if(!obj[char]){
+      obj[char] = 1;
+    }
+    else{
+      obj[char]++
+    }
+  }
+
+  // loop thru an object and compare each key's values
+  for(let char in obj){
+    if(obj[char]>= max){
+      max = obj[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar.toString();
+
+}
 
 module.exports = maxChar;
