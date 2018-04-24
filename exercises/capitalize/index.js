ec9441c7ae21dd90d2a4942a163f653819139657
GrimmOutlook <grimmoutlook87@gmail.com>
2018-04-24 13:25:48 -0400

    Final solution and all tests passing for Capitalize problem.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,58 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const beginArray = str.split(' ');
+  let firstLetter = '';
+  let endOfWord = '';
+  let fullWord = '';
+  const endArray = [];
+
+  for(word of beginArray){
+    endOfWord = word.slice(1);
+    firstLetter = word[0].toUpperCase();
+
+    fullWord = firstLetter + endOfWord;
+    endArray.push(fullWord);
+  }
+
+  console.log('endArray: ', endArray);
+
+  return endArray.join(' ');
+
+}
 
 module.exports = capitalize;
+
+// Alternate solution:
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for(let i = 1; i < str.length; i++){
+//     if(str[i-1] === ' '){
+//       result += str[i].toUpperCase();
+//     }
+//     else{
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
