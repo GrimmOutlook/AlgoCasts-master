1864325c89dd2c21523a79dbff2088f190778bdd
GrimmOutlook <grimmoutlook87@gmail.com>
2018-04-27 12:06:07 -0400

    3 solutions to the Vowels exercise + all tests passing.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,35 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const strLower = str.toLowerCase();
+  let counter = 0;
+  for(let i = 0; i < strLower.length; i++){
+    if(strLower[i] === 'a' || strLower[i] === 'e' || strLower[i] === 'i' || strLower[i] === 'o' || strLower[i] === 'u'){
+      counter++;
+    }
+  }
+  return counter;
+}
 
 module.exports = vowels;
+
+// Alternate Solutions:
+// function vowels(str) {
+//   const strLower = str.toLowerCase();
+//   let counter = 0;
+//   const checkVowel = ['a', 'e', 'i', 'o', 'u'];
+//   for(char of strLower){
+//     if(checkVowel.includes(char)){
+//       counter++;
+//     }
+//   }
+
+//   return counter;
+// }
+
+// function vowels(str) {
+//   const matchArray = str.match(/[aeiou]/gi);
+//   return matchArray === null ? 0 : matchArray.length;
+
+// }
