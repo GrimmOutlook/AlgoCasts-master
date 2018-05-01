40371774c037f51afecc0948cae5fd43728c3d91
GrimmOutlook <grimmoutlook87@gmail.com>
2018-05-01 15:24:14 -0400

    2 solutions to the Chunk exercise + all tests passing.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,15 +8,63 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// empty newArray to push each 'chunk' array into
+// loop thru entire array
+// create a subArray to push each value into
+// once subArray.length === size, push subArray into new Array
+// set subArray to an empty array after pushing.
+
+// ------------ My Solution ----------------------
+// function chunk(array, size) {
+//   const totalArray = [];
+//   let subArray = [];
+//   for(let i = 0; i < array.length; i++){
+//     if(subArray.length < size){
+//       subArray.push(array[i]);
+//     }
+//     else if(subArray.length === size){
+//       totalArray.push(subArray);
+//       subArray = [];
+//       subArray.push(array[i]);
+//     }
+//   }
+//   totalArray.push(subArray);
+//   return totalArray;
+// }
+
+// console.log(chunk([1,2,3,4,5,6,7,8,9], 2));
+
+module.exports = chunk;
+
+//-------------- Alternate Solution ----------------
 function chunk(array, size) {
   const chunked = [];
-  for(let i = 0; i < array.length; i++){
-    newArray.push(array[i]);
+
+  for(let element of array){
+    const last = chunked[chunked.length-1];
+
+    if(!last || last.length === size){
+      chunked.push([element])
+    }
+    else{
+      last.push(element);
+    }
   }
-  // console.log(newArray);
-  return newArray;
+  return chunked;
 }
 
-console.log(chunk([1,2,3,4,5,6,7,8], 2));
 
-// module.exports = chunk;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
